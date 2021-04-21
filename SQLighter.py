cfd4e9ba353371368d136d93ee11a166391a0c70
Ruv1c0n <60100551+Ruv1c0n@users.noreply.github.com>
2021-04-21 23:41:29 +0300

    comments

--- SQLighter.py
+++ SQLighter.py
@@ -2,21 +2,20 @@ import sqlite3
 
 
 class SQLighter:
-
+# Конструктор курсора и привязка к бд
     def __init__(self, database):
         self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
 
-
+# Выбор саундтрека
     def select_single(self, rownum):
         with self.connection:
             return self.cursor.execute('SELECT * FROM music WHERE id = ?', (rownum,)).fetchall()[0]
-
+# Подсчет строк бд для случайного выбора саундтрека
     def count_rows(self):
         with self.connection:
             result = self.cursor.execute('SELECT * FROM music').fetchall()
             return len(result)
-
 
     def select_all(self):
         with self.connection:
