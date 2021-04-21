33890a5892101bd6f5aa2a411e3b43e929813bd7
Ruv1c0n <60100551+Ruv1c0n@users.noreply.github.com>
2021-04-21 23:39:05 +0300

    comments

--- Baze_of_data.py
+++ Baze_of_data.py
@@ -5,15 +5,15 @@ from SQLighter import SQLighter
 from config import database_name, database_name2
 
 slovar = {}
-
+# Считает количество строк в бд
 def count_rows_musiv():
     db = SQLighter(database_name)
     rowsnum = db.count_rows()
     slovar['rows_count1'] = rowsnum
-
+# Регистрация пользовательского имени
 def set_name_from_user(NAME):
     slovar['name'] = NAME
-
+# Использование пользовательского имени
 def get_name_from_user():
     try:
         name = slovar['name']
@@ -21,11 +21,12 @@ def get_name_from_user():
     except KeyError:
         return None
 
+# Считает количество строк в бд для фотографий
 def count_rows_photo():
     db = SQLighter(database_name2)
     rowsnum = db.count_rows()
     slovar['rows_count2'] = rowsnum
-
+# обращается к данным бд по строкам
 def get_rows_count(type_of_cntent):
     if type_of_cntent == 'photo':
         rowsnum = slovar['rows_count2']
@@ -33,15 +34,15 @@ def get_rows_count(type_of_cntent):
         rowsnum = slovar['rows_count1']
     return rowsnum
 
-
+# Записывает ответ пользователя и правильный ответ
 def set_user_game(chat_id, estimated_answer):
     slovar[str(chat_id)] = estimated_answer
 
-
+# Очистка ответов
 def finish_user_game(chat_id):
     del slovar[str(chat_id)]
 
-
+# Получение правильного ответа
 def get_answer_for_user(chat_id):
     try:
         answer = slovar[str(chat_id)]
@@ -50,7 +51,7 @@ def get_answer_for_user(chat_id):
     except KeyError:
         return None
 
-
+# Клавиатура для ответов игры
 def generate_markup(right_answer, wrong_answers):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     all_answers = '{},{}'.format(right_answer, wrong_answers)
